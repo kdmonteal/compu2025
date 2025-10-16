ed490b1d9a0559d3b1a43d2788a9f8bad38482b7
Daniella Marin <36426077+kdmonteal@users.noreply.github.com>
2025-10-16 11:06:02 -0500

    changeSoundFunction

--- Clase 13 Mi Juego/src/js/main.js
+++ Clase 13 Mi Juego/src/js/main.js
@@ -1,15 +1,25 @@
-$(function () {
-    var $jittery = $('.jittery'),
-        aText = $jittery.text().split(''),
-        letters = '';
+var a = 0;
 
-    for (var i = 0; i < aText.length; i++) {
-        letters += '<span>' + aText[i] + '</span>';
+function loadAudio(valueSong) {
+  
+  var audio = document.getElementById('soundScreen');
+
+    switch (valueSong) {
+        case 1:
+            audio.src = './src/sounds/01_game_menu.wav';
+        break;
+        case 2:
+            audio.src = './src/sounds/02_game_background.wav';
+        break;
+        case 3:
+            audio.src = './src/sounds/03_game_win.wav';
+        break;
+        case 4:
+            audio.src = './src/sounds/04_game_over.wav';
+        break;
     }
 
-    $jittery.empty().append(letters);
+  audio.load();
+}
 
-    $.each($('span', $jittery), function (i) {
-        $(this).css('animation-delay', '-' + i + '70ms');
-    });
-});
+loadAudio(3);
